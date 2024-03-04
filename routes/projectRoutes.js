f8d8b207b1508ee0c77e488308d5ded450c2f11d
Rahul Kumar <rahul.web404@gmail.com>
2024-03-04 13:56:02 +0530

    commit reset

--- routes/projectRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const projectController = require('./../controllers/projectController');
-const router = express.Router();
-
-router.route('/project').post(projectController.createProject);
-
-module.exports = router;
